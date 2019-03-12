00bbaeded0961eefa4221cb928a345b10635b77a
ALBERTOPP <46692699+ALBERTOPP@users.noreply.github.com>
2019-03-12 11:01:16 -0600

    Update PCASNPRELATE.R

--- ProyectoUnidad5_AEPP./bin./PCASNPRELATE.R
+++ ProyectoUnidad5_AEPP./bin./PCASNPRELATE.R
@@ -1,11 +1,8 @@
 ######SNPRELATE (PCA DE SNPŚ)#################
-#Instalacion##if (!requireNamespace("BiocManager", quietly = TRUE))
+#Instalacion#
+#if (!requireNamespace("BiocManager", quietly = TRUE))
 #              install.packages("BiocManager")
 #              BiocManager::install("SNPRelate", version = "3.8")
-
-getwd()
-.libPaths()
-.libPaths("/home/alberto/R/x86_64-pc-linux-gnu-library/3.5")
 
 browseVignettes("SNPRelate")
 
@@ -16,23 +13,23 @@ library(ggplt2)
 
 ###Charger the doc.vcf###
 vcf <- "all.mergedSNPs.vcf"
-snpgdsVCF2GDS(vcf,"pro.gds",ignore.chr.prefix="chr_")##Do the *.gds file
-pro.gds <- snpgdsOpen("pro.gds")##Open *.gds file
-snpgdsSummary("pro.gds") ##Summary
+snpgdsVCF2GDS(vcf,"gatos.gds",ignore.chr.prefix="chr_")##Do the *.gds file
+pro.gds <- snpgdsOpen("gatos.gds")##Open *.gds file
+snpgdsSummary("gatos.gds") ##Summary
 
 # Check snp.id and head.id
-head(read.gdsn(index.gdsn(pro.gds, "snp.id")))
-head(read.gdsn(index.gdsn(pro.gds, "sample.id")))
+head(read.gdsn(index.gdsn(gatos.gds, "snp.id")))
+head(read.gdsn(index.gdsn(gatos.gds, "sample.id")))
 
 ###Get sample.id and snp.id
-sample.id <- read.gdsn(index.gdsn(pro.gds, "sample.id"))
+sample.id <- read.gdsn(index.gdsn(gatos.gds, "sample.id"))
 sample.id
 
-snpset.id <- unlist(read.gdsn(index.gdsn(par.gds, "snp.id")))
+snpset.id <- unlist(read.gdsn(index.gdsn(gatos.gds, "snp.id")))
 snpset.id
 ###Do PCA analisis####
 ##PCA for aleles maf >0.5 ##
-pca <- snpgdsPCA(par.gds, num.thread=2, maf = 0.05) ###PCA
+pca <- snpgdsPCA(gatos.gds, num.thread=2, maf = 0.05) ###PCA
 
 ###Percent of variation in EV's#####
 pc.percent <- pca$varprop*100##Porcentaje de variacion
@@ -45,11 +42,11 @@ tab<-data.frame(sample.id=pca$sample.id,##Crear data frame
                  EV2= pca$eigenvect [,2],
                  StringsAsFactors= FALSE)
 ###A new colum whit the legend of sample.id###
-tab=cbind(tab, Grupos= rep(c("RG","DB","Mo","Msr","Tx"), c(60,36,32,31,33))) 
+tab=cbind(tab, Grups= rep(c("RG","DB","Mo","Msr","Tx"), c(60,36,32,31,33))) 
 tab
 
 ####Do de plot in ggplot2#######
-ggplot(data= tab, aes(x=EV2, y=EV1, color=Grupos, shape=Grupos, ))+ geom_point() + 
+ggplot(data= tab, aes(x=EV2, y=EV1, color=Grups, shape=Grups, ))+ geom_point() + 
   ylab(paste0("eigenvector 1 explainning ", round(pc.percent, 2) [1],"%")) +
   xlab(paste0("eigenvector 2 explainning ", round(pc.percent, 2) [2],"%")) +
   theme_bw()
